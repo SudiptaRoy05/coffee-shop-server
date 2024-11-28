1c6d3912af92ce631a39eaca87d1a1e8fda2289d
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-11-28 12:36:57 +0600

    data recieve form client and sent to db

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lue0n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +22,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+    const database = client.db("coffeeDB");
+    const coffeeCollection = database.collection("coffeeCollection");
+
+    app.post('/coffee', async(req,res)=>{
+      const newCoffee = req.body
+      console.log(newCoffee)
+      const result = await coffeeCollection.insertOne(newCoffee);
+      console.log(result)
+      res.send(result);
+
+    })
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
